7dec6967aa48cde47ea332e8ce7106022941f553
mike.rettig <mike.rettig@838d0db3-8425-0410-a4eb-09b97d8a4dc5>
2007-03-25 02:21:21 +0000

    add pattern matching subscriber.

---
+++ Emcaster4j/src/com/emcaster/topics/MessageListener.java
@@ -0,0 +1,7 @@
+package com.emcaster.topics;
+
+public interface MessageListener {
+
+	void onMessage(Message msg);
+	
+}
---
+++ Emcaster4j/src/com/emcaster/topics/PatternListener.java
@@ -0,0 +1,23 @@
+package com.emcaster.topics;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternListener implements MessageListener {
+	
+	private final Pattern _pattern;
+	private final MessageListener _listener;
+	
+	public PatternListener(Pattern pattern, MessageListener listener){
+		_pattern = pattern;
+		_listener = listener;
+	}
+	
+	public void onMessage(Message msg){
+		Matcher matcher = _pattern.matcher(msg.getTopic());
+		if(matcher.matches()){
+			_listener.onMessage(msg);
+		}
+	}
+
+}
