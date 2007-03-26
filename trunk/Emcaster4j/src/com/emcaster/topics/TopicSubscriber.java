9891beeac98d259b223cf2ad5cc9ab7d43b8f27f
mike.rettig <mike.rettig@838d0db3-8425-0410-a4eb-09b97d8a4dc5>
2007-03-26 05:57:33 +0000

    remove checked exceptions.

--- trunk/Emcaster4j/src/com/emcaster/topics/TopicSubscriber.java
+++ trunk/Emcaster4j/src/com/emcaster/topics/TopicSubscriber.java
@@ -1,11 +1,10 @@
 package com.emcaster.topics;
 
-import java.io.IOException;
 import java.util.Iterator;
 
 public interface TopicSubscriber {
 
-	void start() throws IOException;	
-	Iterator<Message> readNext() throws IOException;
+	void connect();	
+	Iterator<Message> readNext();
 
 }
--- trunk/Emcaster4j/src/com/emcaster/topics/UdpSubscriber.java
+++ trunk/Emcaster4j/src/com/emcaster/topics/UdpSubscriber.java
@@ -11,44 +11,66 @@ import java.util.Iterator;
 public class UdpSubscriber implements TopicSubscriber {
 
 	private final String _address;
+
 	private final int _port;
+
 	private final ByteBuffer _buffer;
+
 	private final MulticastSocket _socket;
+
 	private final MessageParserImpl _parser;
+
 	private final DatagramPacket _packet;
+
 	private final MessageIterator _iterator;
-	
-	public UdpSubscriber(String address, int port, int bufferSize) throws IOException{
+
+	public UdpSubscriber(String address, int port, int bufferSize) {
 		_address = address;
 		_port = port;
 		_buffer = ByteBuffer.allocate(bufferSize);
 		_buffer.order(ByteOrder.LITTLE_ENDIAN);
-		_socket = new MulticastSocket(_port);		
+		try {
+			_socket = new MulticastSocket(_port);
+		} catch (IOException e) {
+			throw new InvalidSocketException(e);
+		}
 		_packet = new DatagramPacket(_buffer.array(), _buffer.capacity());
 		_parser = new MessageParserImpl(_buffer, _packet);
 		_iterator = new MessageIterator(_parser);
 	}
-	
-	public MulticastSocket getSocket(){
+
+	public MulticastSocket getSocket() {
 		return _socket;
 	}
-	
-	public void start() throws IOException{
-		InetAddress inet = InetAddress.getByName(_address);
-		_socket.joinGroup(inet);
+
+	public void connect() {
+		try {
+			InetAddress inet = InetAddress.getByName(_address);
+			_socket.joinGroup(inet);
+		} catch (IOException exc) {
+			throw new InvalidSocketException(exc);
+		}
 	}
 
-	public Iterator<Message> readNext() throws IOException{
-		_socket.receive(_packet);
+	public Iterator<Message> readNext() {
+		try {
+			_socket.receive(_packet);
+		} catch (IOException e) {
+			throw new InvalidSocketException(e);
+		}
 		_iterator.setLength(_packet.getLength());
 		_buffer.rewind();
 		return _iterator;
 	}
-	
-	public void stop() throws IOException{
-		InetAddress inet = InetAddress.getByName(_address);
-		_socket.leaveGroup(inet);
-		_socket.close();
+
+	public void stop() {
+		try {
+			InetAddress inet = InetAddress.getByName(_address);
+			_socket.leaveGroup(inet);
+			_socket.close();
+		} catch (IOException exc) {
+			throw new InvalidSocketException(exc);
+		}
 	}
-	
+
 }
